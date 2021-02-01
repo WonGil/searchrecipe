1804ec176912a1a7260828e5a7a0f100f0cb49be
paladin00 <paladin2001@hanmail.net>
2021-02-01 17:37:31 +0900

    Revert "모델오차 수정 및 Handler 수정"
    This reverts commit fa2301af

--- order/src/main/java/searchrecipe/PolicyHandler.java
+++ order/src/main/java/searchrecipe/PolicyHandler.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PolicyHandler{
     @StreamListener(KafkaProcessor.INPUT)
@@ -19,6 +21,12 @@ public class PolicyHandler{
     public void wheneverShipped_UpdateStatus(@Payload Shipped shipped){
 
         if(shipped.isMe()){
+            Optional<Order> orderOptional = orderRepository.findById(shipped.getOrderId());
+            Order order = orderOptional.get();
+            order.setStatus(shipped.getStatus());
+
+            orderRepository.save(order);
+
             System.out.println("##### listener  : " + shipped.toJson());
         }
     }
